1f2797c6e2bd7929438e31e05fcc4352ed57e8ec
Tamara Cajiao <167063588+TamaraCM06@users.noreply.github.com>
2026-01-05 22:17:40 -0600

    exit button hover

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -6,9 +6,11 @@ extends Panel
 @onready var house: Button = $House
 @onready var bakery: Button = $Bakery
 @onready var park: Button = $Park
+@onready var exit: Button = $Exit
 @onready var glow_house: Sprite2D = $GlowHouse
 @onready var glow_bakery: Sprite2D = $GlowBakery
 @onready var glow_park: Sprite2D = $GlowPark
+@onready var fantasma: Sprite2D = $Fantasma
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -20,14 +22,22 @@ func _process(delta: float) -> void:
 	glow_house.visible = false
 	glow_bakery.visible = false
 	glow_park.visible = false
-
 	if house.is_hovered():
+		buttonHovered(house, glow_house)
+	else:
 		buttonHovered(house, glow_house)
 	if bakery.is_hovered():
 		buttonHovered(bakery, glow_bakery)
+	else:
+		buttonHovered(bakery, glow_bakery)
 	if park.is_hovered():
 		buttonHovered(park, glow_park)
-	
+	else:
+		buttonHovered(park, glow_park)
+	if exit.is_hovered():
+		buttonHovered(exit, fantasma)
+	else:
+		buttonHovered(exit, fantasma)
 
 func startTween(object: Object, property: String, final_val: Variant, duration: float):
 	var tween = create_tween()
